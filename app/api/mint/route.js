3c57efc39d63f698f8e2b3a293f97eb081996f61
zoey-t <101599447+zoey-t@users.noreply.github.com>
2024-03-16 04:37:36 +0000

    add mint/transfer related api code

---
+++ app/api/mint/route.js
@@ -0,0 +1,30 @@
+export async function POST(req) {
+  console.log("handle POST mint")
+  try {
+    // Extract mintData from the request body
+    const { mintData } = req.body;
+
+    // Process mintData (e.g., mint tokens)
+    // Your minting logic goes here...
+
+    // If minting is successful, send a success response
+    // return new Response(
+    //   {message: 'server:Token minted successfully',},
+    //   {status: 200}
+    // )
+
+    return Response.json(
+      {message:"server:Token minted successfully"},
+    {status: 200,
+      
+    })
+  } catch (error) {
+    console.log(error)
+    return Response.json(
+      {message:  error.message|"Internal server error",},
+      {status: 200,
+      }
+    )
+
+  }
+  }
